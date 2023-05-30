2a555825828213b9c3fa4692ee8863ae15cb254e
ThiagoDonas <thidonas@gmail.com>
2023-05-30 17:46:50 -0300

    fix: remove mask

--- src/stories/components/Forms/InputText/types.ts
+++ src/stories/components/Forms/InputText/types.ts
@@ -1,16 +1,6 @@
 import { ReactNode } from 'react';
 
-type MaskType = 'register' | 'date' | 'phone' | 'zipcode' | 'onlyNumbers';
-
-export interface MaskProps {
-  onChange: (event: { target: { name: string; value: unknown } }) => void;
-  name: string;
-  value: string;
-  mask?: MaskType;
-}
-
 export interface InputProps {
-  maskType?: MaskType;
   leftIcon?: boolean;
   rigthIcon?: boolean;
   icon?: ReactNode;
