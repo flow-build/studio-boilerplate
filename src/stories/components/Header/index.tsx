a87235bbe752609a448afe5f4b54d1259ec767ec
gabriela sales <gabriela.coutinho.sales@gmail.com>
2023-05-31 12:20:50 -0300

    [FS-190] feat: adjust header

--- src/stories/components/Header/index.tsx
+++ src/stories/components/Header/index.tsx
@@ -1,50 +1,33 @@
 'use client';
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
 import * as S from './styles';
 import { HeaderProps } from './types';
 import { Button } from '@mui/material';
 
 export const Header: React.FC<HeaderProps> = ({ logo, links, button, loggedIn, username }) => {
-  if (loggedIn ?? false) {
-    return (
-      <AppBar position="static" color="default">
-        <S.ToolbarHeader>
-          {logo}
-          <S.ContainerHeaderLogged>
-            {links?.map((link, index) => (
-              <React.Fragment key={link.name}>
-                <Typography>
-                  <S.LinkHeader href={link.url}>{link.name}</S.LinkHeader>
-                </Typography>
-                {index !== links.length - 1 && <S.DividerHeader>|</S.DividerHeader>}
-              </React.Fragment>
-            ))}
-          </S.ContainerHeaderLogged>
-          {username != null && (
-            <Button color="inherit" variant="outlined" size="large" onClick={button.onClick}>
-              {username}
-            </Button>
-          )}
-        </S.ToolbarHeader>
-      </AppBar>
-    );
-  }
   return (
     <AppBar position="static" color="default">
       <S.ToolbarHeader>
         {logo}
-        <S.ContainerHeader>
+        <S.ContainerHeader loggedIn={loggedIn ?? false}>
           {links?.map((link, index) => (
-            <Typography key={link.name}>
+            <S.DividedLink loggedIn={loggedIn ?? false} key={link.name}>
               <S.LinkHeader href={link.url}>{link.name}</S.LinkHeader>
-            </Typography>
+            </S.DividedLink>
           ))}
         </S.ContainerHeader>
-        <Button color="inherit" variant="outlined" size="large" onClick={button.onClick}>
-          {button.name}
-        </Button>
+        {loggedIn === true ? (
+          username != null && (
+            <Button color="inherit" variant="outlined" size="large" onClick={button.onClick}>
+              {username}
+            </Button>
+          )
+        ) : (
+          <Button color="inherit" variant="outlined" size="large" onClick={button.onClick}>
+            {button.name}
+          </Button>
+        )}
       </S.ToolbarHeader>
     </AppBar>
   );
--- src/stories/components/Header/styles.ts
+++ src/stories/components/Header/styles.ts
@@ -1,19 +1,23 @@
-import { Container, Divider, Toolbar } from '@mui/material';
+import { Container, Divider, Toolbar, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Link from 'next/link';
 import { fontSizes } from 'theme/fontSizes.theme';
 import { spacing } from 'theme/spacing';
+
+interface Props {
+  loggedIn: boolean;
+}
 
 export const ToolbarHeader = styled(Toolbar)`
   display: flex;
   justify-content: space-between;
 `;
 
-export const ContainerHeader = styled(Container)`
-  display: flex;
-  justify-content: flex-end;
-  gap: ${spacing.S16};
-`;
+export const ContainerHeader = styled(Container)<Props>(({ loggedIn }) => ({
+  display: 'flex',
+  justifyContent: loggedIn ?? true ? 'center' : 'flex-end',
+  gap: loggedIn ?? false ? 'initial' : `${spacing.S16}`
+}));
 
 export const ContainerHeaderAvatar = styled(Container)`
   display: flex;
@@ -25,8 +29,17 @@ export const ContainerHeaderLogged = styled(Container)`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: ${spacing.S16};
 `;
+
+export const DividedLink = styled(Typography)<Props>(({ loggedIn }) => ({
+  '::after': {
+    content: loggedIn ?? false ? '"|"' : 'none',
+    margin: loggedIn ?? false ? `${spacing.S10}` : '0'
+  },
+  ':last-child::after': {
+    content: loggedIn ?? false ? '""' : 'none'
+  }
+}));
 
 export const LinkHeader = styled(Link)`
   color: black;
