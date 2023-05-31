43f27bf7243c71ce925c7c9877740aa1466784fe
Elson Oliveira <elson.oliveira@fdte.io>
2023-05-31 10:07:26 -0300

    refactor: remove instant mask due to formatter implementation

--- src/utils/masks/currency/instant/index.ts
+++
@@ -1,8 +0,0 @@
-export const maskCurrencyInstant = (value: number) => {
-  if (isNaN(value)) {
-    return '';
-  }
-
-  const options = { currency: 'BRL', style: 'currency' };
-  return new Intl.NumberFormat('pt-BR', options).format(value);
-};
