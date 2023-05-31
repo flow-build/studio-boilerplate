19c21e42a052c632bd180092a263e4ba2b9220e7
Felipe Siqueira <flipggs@gmail.com>
2023-05-31 18:08:46 -0300

    chore: implemented headers in post request

--- src/services/httpClient/index.ts
+++ src/services/httpClient/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Logger from 'utils/logger';
 
 interface Error {
@@ -7,14 +8,24 @@ interface Error {
 
 function API() {
   let BASE_URL = '';
+  let HEADER: HeadersInit;
 
   return {
     setBaseUrl(baseUrl: string) {
       BASE_URL = baseUrl;
     },
+    setHeader(header: HeadersInit) {
+      HEADER = header;
+    },
     async get<T>(url: string, options?: RequestInit): Promise<T | Error> {
       try {
-        const response = await fetch(`${BASE_URL}${url}`, options);
+        const response = await fetch(`${BASE_URL}${url}`, {
+          ...options,
+          headers: {
+            ...options?.headers,
+            ...HEADER
+          }
+        });
         const result = await response.json();
 
         return result as T;
@@ -26,11 +37,17 @@ function API() {
         };
       }
     },
-    async post<T>(url: string, options?: RequestInit): Promise<T | Error> {
+    async post<T>(url: string, data?: any, options?: RequestInit): Promise<T | Error> {
       try {
+        const body: string | undefined = data ? JSON.stringify(data) : undefined;
         const response = await fetch(`${BASE_URL}${url}`, {
           method: 'POST',
-          ...options
+          ...options,
+          body,
+          headers: {
+            ...options?.headers,
+            ...HEADER
+          }
         });
         const result = await response.json();
 
