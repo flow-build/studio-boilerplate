6ad8511656c3ea25fc6d158d4c4cc82c58144a30
ThiagoDonas <thidonas@gmail.com>
2023-05-31 12:08:09 -0300

    fix: change style

--- src/stories/components/Forms/InputText/styles.ts
+++ src/stories/components/Forms/InputText/styles.ts
@@ -2,6 +2,5 @@ import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 
 export const InputText = styled(TextField)`
-  max-width: 37.5rem;
   width: 100%;
 `;
