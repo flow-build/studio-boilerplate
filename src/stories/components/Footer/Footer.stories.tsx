1309a4789f2555ea61a680ef1a18e3785d90b9ab
gabriela sales <gabriela.coutinho.sales@gmail.com>
2023-06-01 11:34:46 -0300

    [FS-191] fix: footer

--- src/stories/components/Footer/Footer.stories.tsx
+++ src/stories/components/Footer/Footer.stories.tsx
@@ -53,6 +53,7 @@ export const Standard: StoryFn<typeof Footer> = (args) => {
 
   return (
     <Footer
+      {...args}
       navLinks={navLinks}
       socialMedias={socialMedias}
       disclaimer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
