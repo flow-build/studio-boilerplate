a67295730d3f10306b4566c74b649296bcfc96c9
ThiagoDonas <thidonas@gmail.com>
2023-06-01 12:46:14 -0300

    fix: error lint

--- src/stories/components/Breadcrumb/ItemsBreadcrumb/index.tsx
+++ src/stories/components/Breadcrumb/ItemsBreadcrumb/index.tsx
@@ -1,13 +1,14 @@
 import React, { useMemo } from 'react';
-import { ItemsBreadcrumbProps } from './types';
+
 import * as S from './styles';
+import { ItemsBreadcrumbProps } from './types';
 
 export const ItemsBreadcrumb = ({ items }: ItemsBreadcrumbProps) => {
   const breadcrumbs = useMemo(() => {
     if (items.length > 0) {
       return items?.map((item, index) => {
         const isLastItem = index + 1 === items.length;
-        if (isLastItem && !item.redirectLink) {
+        if (isLastItem && item.redirectLink !== undefined) {
           return <S.LastText key={item.text}>{item.text}</S.LastText>;
         }
 
