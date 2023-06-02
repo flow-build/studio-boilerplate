17da111721bf87869955e5abb09e2e4b80d4a53f
Elson Oliveira <elson.oliveira@fdte.io>
2023-06-02 11:18:58 -0300

    fix: remove index from key property

--- src/stories/components/Forms/MultiSelect/components/ChipList/index.tsx
+++ src/stories/components/Forms/MultiSelect/components/ChipList/index.tsx
@@ -6,8 +6,8 @@ import * as S from './styles';
 export const ChipList: React.FC<ChipListProps> = ({ options }) => {
   return (
     <S.Wrapper>
-      {options.map((option, index) => (
-        <Chip key={index.toString()} label={option.label} />
+      {options.map((option) => (
+        <Chip key={`${option.value}-${option.label}`} label={option.label} />
       ))}
     </S.Wrapper>
   );
