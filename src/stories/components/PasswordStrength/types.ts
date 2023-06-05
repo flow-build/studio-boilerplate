145c4e2def0b7d7bb19fb6ae00eff0400dbc12c6
gabriela sales <gabriela.coutinho.sales@gmail.com>
2023-06-05 18:33:48 -0300

    [FS-184] feat: create progress bar

---
+++ src/stories/components/PasswordStrength/types.ts
@@ -0,0 +1,8 @@
+export interface ProgressBarProps {
+  descriptionTip: string;
+  strength: 'weak' | 'medium' | 'strong' | 'great' | '';
+}
+
+export interface StyledTips {
+  strength: 'weak' | 'medium' | 'strong' | 'great' | '';
+}
