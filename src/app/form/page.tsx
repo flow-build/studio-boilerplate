55b5f97c156033539b7e669e1fb4e1b93320c351
gabriela sales <gabriela.coutinho.sales@gmail.com>
2023-06-06 15:24:29 -0300

    [FS-200] feat: add validator

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -3,11 +3,10 @@
 import { FormEvent } from 'react';
 
 import Form, { IChangeEvent } from '@rjsf/core';
+import validator from '@rjsf/validator-ajv8';
 import { JSONSchema7 } from 'json-schema';
 import { InputText } from 'stories/components';
 import { Button } from 'stories/components/Forms/Button';
-import { ValidationError } from 'yup';
-import * as yup from 'yup';
 
 import formSchema from './formSchema.json';
 import * as S from './styles';
@@ -48,32 +47,13 @@ export default function MyForm() {
     formik.handleSubmit(e);
   };
 
-  const validate = async (formData: FormData, errors: ValidationError) => {
-    const validationSchema = yup.object().shape({
-      name: yup.string().required('Campo obrigatório'),
-      email: yup.string().required('Campo obrigatório').email('E-mail inválido')
-    });
-
-    try {
-      await validationSchema.validate(formData, { abortEarly: false });
-    } catch (validationError) {
-      validationError.inner.forEach((error) => {
-        if (errors[error.path]) {
-          errors[error.path].addError(error.message);
-        }
-      });
-    }
-
-    return errors;
-  };
-
   return (
     <S.Wrapper>
       <Form
         schema={formSchema as JSONSchema7}
         widgets={widgets}
         onSubmit={handleFormSubmit}
-        validator={validate}
+        validator={validator}
       >
         <Button type="submit" variant="contained" color="primary">
           Submit
