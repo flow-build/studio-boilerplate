911ace7b43130c6a17f274c302148b1db1a226b7
Elson Oliveira <elson.oliveira@fdte.io>
2023-06-06 11:17:28 -0300

    feat: token page created

---
+++ src/app/token/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ImageComponent } from 'stories/components';
+import { Button } from 'stories/components/Forms/Button';
+import { OTPInput } from 'stories/components/Forms/OTPInput';
+
+import * as S from './styles';
+
+export default function ForgotPassword() {
+  return (
+    <S.Main>
+      <S.Wrapper>
+        <S.SideLeft>
+          <ImageComponent
+            src="https://img.logoipsum.com/263.svg"
+            alt="Logo"
+            width={150}
+            height={30}
+          />
+        </S.SideLeft>
+
+        <S.Form>
+          <h3>Validação</h3>
+          <p>Insira o token recebido pelo e-mail</p>
+
+          <OTPInput autoFocus length={5} onChangeToken={(token) => console.log({ token })} />
+
+          <Button type="submit" variant="outlined">
+            Enviar
+          </Button>
+        </S.Form>
+      </S.Wrapper>
+    </S.Main>
+  );
+}
