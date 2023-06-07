0889943f35b204e4cc4292f7e1ad19adbc9cedf8
ThiagoDonas <thidonas@gmail.com>
2023-06-06 21:14:45 -0300

    fix: change styles

--- src/stories/components/RecentPosts/styles.ts
+++ src/stories/components/RecentPosts/styles.ts
@@ -1,22 +1,25 @@
 import { Typography, styled } from '@mui/material';
 import { spacing } from 'theme/spacing';
 export const Wrapper = styled('div')`
-  color: ${({ theme }) => theme.palette.primary.main};
+  background-color: ${({ theme }) => theme.palette.text.disabled};
+  padding: 0.8rem;
+
+  width: 25%;
 `;
 
 export const Title = styled(Typography)`
-  color: ${({ theme }) => theme.palette.grey['700']};
+  color: ${({ theme }) => theme.palette.text.primary};
   font-size: ${({ theme }) => theme.typography.h6};
   font-weight: bold;
   margin-bottom: ${spacing.S14};
 `;
 
 export const Price = styled(Typography)`
-  color: ${({ theme }) => theme.palette.grey['500']};
+  color: ${({ theme }) => theme.palette.text.secondary};
   font-size: ${({ theme }) => theme.typography.subtitle1};
 `;
 
 export const Description = styled(Typography)`
-  color: ${({ theme }) => theme.palette.grey['500']};
+  color: ${({ theme }) => theme.palette.text.secondary};
   font-size: ${({ theme }) => theme.typography.subtitle2};
 `;
