e289dbc7491c49fec0f80dcdd76a42358c38fc26
Elson Oliveira <elson.oliveira@fdte.io>
2023-06-07 11:15:06 -0300

    feat: submit email for forgot password

--- src/app/forgot-password/useForgotPassword.ts
+++ src/app/forgot-password/useForgotPassword.ts
@@ -1,8 +1,17 @@
+import { useDispatch } from 'react-redux';
+
 import { useFormik } from 'formik';
+import _delay from 'lodash/delay';
+import { useRouter } from 'next/navigation';
+import api from 'services/httpClient';
+import { setEmail, setResettingPassword } from 'store/slices/user';
 import { Logger } from 'utils';
 import * as yup from 'yup';
 
 export const useForgotPassword = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
   const INITIAL_VALUES = {
     email: ''
   };
@@ -15,8 +24,22 @@ export const useForgotPassword = () => {
     initialValues: INITIAL_VALUES,
     validationSchema,
     validateOnBlur: true,
-    onSubmit: Logger.info
+    onSubmit
   });
+
+  async function onSubmit(values: typeof INITIAL_VALUES) {
+    dispatch(setResettingPassword(values.email));
+    _delay(() => router.push('/reset-password'), 500);
+
+    /* const result = await api.post('/api/forgotPassword/sendCode', { email: values.email });
+    console.log({ result });
+
+    if (result?.status === 200) {
+      _delay(() => router.push('/verify-email'), 500);
+    } else {
+      Logger.error('error resetting password');
+    } */
+  }
 
   return { formik };
 };
