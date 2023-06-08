08c036a3a585f8e733a243faba03eed6861477c3
Felipe Siqueira <flipggs@gmail.com>
2023-06-08 09:02:36 -0300

    chore: remove mock

--- src/stories/components/ElasticSearch/SearchUI/SearchUI.stories.tsx
+++ src/stories/components/ElasticSearch/SearchUI/SearchUI.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { mockCategories } from 'stories/components/Carousel/mockResult';
 
 import { SearchUI } from './';
-import { congigSearchUI, configFields, congigSearchUI2, configFields2 } from './mock';
+import { congigSearchUI, configFields } from './mock';
 
 const meta: Meta<typeof SearchUI> = {
   title: 'Elastic/SearchUI',
@@ -18,18 +18,6 @@ export const Primary: Story = {
     title: 'Games',
     config: congigSearchUI,
     configFields,
-    carouselItems: {
-      data: mockCategories,
-      slidesPerView: 10
-    }
-  }
-};
-
-export const Secondary: Story = {
-  args: {
-    title: 'Games',
-    config: congigSearchUI2,
-    configFields: configFields2,
     carouselItems: {
       data: mockCategories,
       slidesPerView: 10
